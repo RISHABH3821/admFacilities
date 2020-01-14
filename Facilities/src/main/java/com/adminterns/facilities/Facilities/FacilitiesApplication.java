3b8daf71f0dd400b2b56b56646d957df6a785b59
Rishabh Shrivastav <rishrivastav@BLR.ALLEGISINDIA.COM>
2020-01-14 11:56:50 +0530

    initialization

---
+++ Facilities/src/main/java/com/adminterns/facilities/Facilities/FacilitiesApplication.java
@@ -0,0 +1,13 @@
+package com.adminterns.facilities.Facilities;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FacilitiesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FacilitiesApplication.class, args);
+	}
+
+}
---
+++ Facilities/src/test/java/com/adminterns/facilities/Facilities/FacilitiesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.adminterns.facilities.Facilities;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FacilitiesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
